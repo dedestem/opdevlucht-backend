0151f7b6a9863421bc0fada5c48788c9a2e8a116
dedestem <david@davidnet.net>
2025-07-12 14:51:42 +0200

    Patches and bandages

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
-CREATE USER IF NOT EXISTS 'opdevlucht'@'%' IDENTIFIED BY 'db_password';
+ALTER USER 'opdevlucht'@'%' IDENTIFIED WITH mysql_native_password BY 'db_password';
 GRANT ALL PRIVILEGES ON opdevlucht_db.* TO 'opdevlucht'@'%';
 FLUSH PRIVILEGES;
