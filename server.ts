d39db30128e6d9be11b5feac6b4641fcb7777d14
dedestem <david@davidnet.net>
2025-07-12 13:31:31 +0200

    Allow *

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const app = new Application();
 const router = new Router();
+
+// Enable CORS for all routes and origins
+app.use(oakCors()); // <- This enables CORS with default settings (allow all origins)
 
 // Root route
 router.get("/", (ctx) => {
